c656705851af18c32c7cb7f604422013a7742c6a
John Ghavamzadeh <johnghav@outlook.com>
2014-12-08 15:42:26 +0100

    back up of old website

---
+++ OLD/sidebar.php
@@ -0,0 +1,33 @@
+<div id="sidebar">
+
+<div id="menu">
+
+	<div class="sidebar-content">
+	<input class="input-search" placeholder="Search" />
+	</div>
+
+	<div class="menu-title">
+	Categories
+	</div>
+	<ul>
+		<li><a href="#">World</a></li>
+		<li><a href="#">Politics</a></li>
+		<li><a href="#">Business</a></li>
+		<li><a href="#">Technology</a></li>
+		<li><a href="#">Science</a></li>
+		<li><a href="#">Health</a></li>
+		<li><a href="#">Sports</a></li>
+	</ul>
+	
+	<div class="menu-title">
+	Pages
+	</div>
+	<ul>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Help</a></li>
+		<li><a href="#">Developers</a></li>
+	</ul>
+	
+</div>
+
+</div>
---
+++ OLD/v2/footer.php
@@ -0,0 +1,38 @@
+
+		</div>
+	</div>
+
+<footer>
+	<div class="copy">
+	<ul>
+	<li>&copy; 2014 Dinero</li>
+	<li><a href="#">Om</a></li>
+	<li><a href="#">Hj&auml;lp</a></li>
+	<li><a href="#">Blogg</a></li>
+	<li><a href="#">Regler</a></li>
+	<li><a href="#">Integritet</a></li>
+	<li><a href="#">Cookies</a></li>
+	<li><a href="#">Annonsinformation</a></li>
+	<li><a href="#">Annosera</a></li>
+	<li><a href="#">Media</a></li>
+	<li><a href="#">Utvecklare</a></li>
+	</ul>
+	</div>
+
+	<div class="top"><a href="#" class="go-top">Top</a></div>
+</footer>
+
+	<script>
+		$(document).ready(function() {
+			
+			// Animate the scroll to top
+			$('.go-top').click(function(event) {
+				event.preventDefault();
+				
+				$('html, body').animate({scrollTop: 0}, 300);
+			})
+		});
+	</script>
+	
+</body>
+</html>
