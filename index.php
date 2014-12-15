ed56a6c47109e38f563daf4d2d3d4d76cf6eccfb
John Ghavamzadeh <johnghav@outlook.com>
2014-12-15 14:21:36 +0100

    lol

--- index.php
+++ index.php
@@ -11,43 +11,33 @@
 
 <?php
 
-                require_once('src/Basho/Riak/Riak.php');
-                require_once('src/Basho/Riak/Bucket.php');
-                require_once('src/Basho/Riak/Exception.php');
-                require_once('src/Basho/Riak/Link.php');
-                require_once('src/Basho/Riak/MapReduce.php');
-                require_once('src/Basho/Riak/Object.php');
-                require_once('src/Basho/Riak/StringIO.php');
-                require_once('src/Basho/Riak/Utils.php');
-                require_once('src/Basho/Riak/Link/Phase.php');
-                require_once('src/Basho/Riak/MapReduce/Phase.php');
+		/* BOX WRAPPER #1*/
+		echo '<div class="ctweet">';
 
-                                $client = new Basho\Riak\Riak('172.31.32.109',10018);
-                                $myBucket = $client->bucket('world');
-                                $keys = $myBucket->getKeys();
+		/*PROFILE IMAGE*/
+		echo '<div class="profileimage">';		
+		echo '</div>';
+		
+		/*DIV FOR BOX*/
+        echo '<div class="ctheader">';
+		
+		echo '<a class="ttitel" href="http://twitter.com/';
+        echo 'NAMN';
+		echo '">';
+        echo 'NAMN';
+		echo '</a>';
+		
+		echo '<div class="datum">';
+		echo 'DATUM';
+		
+		echo '</div></div>';
+		
+        echo '<div class="tcontent">';
+        echo '<p>Proin id feugiat orci, ut scelerisque magna. Vestibulum rutrum pretium quam eu laoreet.</p>';
+        echo '</div>';
 
-                for($x=0; $x<=sizeof($keys); $x++) {
-                $fetched = $myBucket->get($keys[$x]);
-                $data = $fetched->getData();
-
-echo '<div class="ctweet">';
-
-        echo '<div class="ctheader"><div class="ct"><a class="ttitel" href="';
-        echo '">';
-        echo $data['description']. "\n";
-        echo '</a></div>';
-
-        echo '<div class="datum">';
-        echo $data['created_at']."\r\n";
-        echo '</div></div>';
-
-
-        echo '<div class="tcontent"><p>';
-        echo $data['text']. "<br>";
-        echo '</p></div>';
-
-echo '</div>';
-}
+		/* BOX WRAPPER END #1 */
+		echo '</div>';
 ?>
 
 </div>
