8546081c1d6a856390659f9588673fcf45a142af
John Ghavamzadeh <johnghav@outlook.com>
2014-12-18 14:20:02 +0100

    fuck all yo hoes

--- sidebar.php
+++ sidebar.php
@@ -4,13 +4,21 @@
 
 	<ul>		
 		<li><a href="socialdemokraterna.php">Socialdemokraterna</a></li>
+		<li class="sep"></li>
 		<li><a href="moderaterna.php">Moderaterna</a></li>
+		<li class="sep"></li>
 		<li><a href="sverigedemokraterna.php">Sverigedemokraterna</a></li>
+		<li class="sep"></li>
 		<li><a href="miljopartiet.php">Miljöpartiet</a></li>
+		<li class="sep"></li>
 		<li><a href="centerpartiet.php">Centerpartiet</a></li>
+		<li class="sep"></li>
 		<li><a href="vansterpartiet.php">Vänsterpartiet</a></li>
+		<li class="sep"></li>
 		<li><a href="folkpartiet.php">Folkpartiet</a></li>
+		<li class="sep"></li>
 		<li><a href="kristdemokraterna.php">Kristdemokraterna</a></li>
+		<li class="sep"></li>
 	</ul>
 
 	
